3721145d3427c692a9b5f8999fc12146747e582c
Daniel Mancilla <dm0275@gmail.com>
2024-08-05 13:28:10 -0500

    Updated magefile

--- magefile.go
+++ magefile.go
@@ -12,9 +12,14 @@ type ArgoConfig struct {
 	Version string
 }
 
-var config = ArgoConfig{
-	Version: "v2.11.3", // use `stable` for the latest version
-}
+var (
+	argocdHost    = "localhost:8080"
+	gitOpsRepo    = "https://github.com/dm0275/argo-gitops.git"
+	gitOpsRepoSsh = "git@github.com:dm0275/argo-gitops.git"
+	config        = ArgoConfig{
+		Version: "v2.11.3", // use `stable` for the latest version
+	}
+)
 
 // Creates the argocd namespace and installs argocd
 func InstallArgo() error {
@@ -72,6 +77,80 @@ func ArgoLogin() error {
 	output, err := run(fmt.Sprintf("argocd login --username admin --password %s --insecure localhost:8080", adminPass))
 	if err != nil {
 		return fmt.Errorf("unable to login. ERROR: %s", err)
+	}
+
+	fmt.Println(output)
+
+	return nil
+}
+
+// Add github ssh cert
+func AddKnownHosts() error {
+	// Add github ssh cert
+	output, err := run("ssh-keyscan github.com | argocd cert add-ssh --batch")
+	if err != nil {
+		return fmt.Errorf("unable add github ssh cert. ERROR: %s", err)
+	}
+
+	fmt.Println(output)
+
+	return nil
+}
+
+// Add Argo repo credentials
+func AddRepoCreds() error {
+	// Add repocreds
+	output, err := run("argocd repocreds add git@github.com --ssh-private-key-path ~/.ssh/id_rsa")
+	if err != nil {
+		return fmt.Errorf("unable add repocreds. ERROR: %s", err)
+	}
+
+	fmt.Println(output)
+
+	return nil
+}
+
+func AddRepo() error {
+	// Add new repo
+	output, err := run(fmt.Sprintf("argocd repo add %s --server %s", gitOpsRepo, argocdHost))
+	if err != nil {
+		return fmt.Errorf("unable add repository. ERROR: %s", err)
+	}
+
+	fmt.Println(output)
+
+	return nil
+}
+
+func AddRepoSsh() error {
+	// Add new repo
+	output, err := run(fmt.Sprintf("argocd repo add %s --ssh-private-key-path ~/.ssh/id_rsa --server %s", gitOpsRepoSsh, argocdHost))
+	if err != nil {
+		return fmt.Errorf("unable add repository. ERROR: %s", err)
+	}
+
+	fmt.Println(output)
+
+	return nil
+}
+
+func CreateAppCli() error {
+	// Add new app via argocd cli
+	output, err := run(fmt.Sprintf("argocd app create app1 --repo %s --path applications/1-directory --dest-server https://kubernetes.default.svc --dest-namespace app1", gitOpsRepoSsh))
+	if err != nil {
+		return fmt.Errorf("unable add application. ERROR: %s", err)
+	}
+
+	fmt.Println(output)
+
+	return nil
+}
+
+func CreateAppManifest() error {
+	// Add new app via manifest
+	output, err := run("kubectl apply -f applications/application.yaml")
+	if err != nil {
+		return fmt.Errorf("unable add application. ERROR: %s", err)
 	}
 
 	fmt.Println(output)
